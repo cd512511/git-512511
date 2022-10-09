b23015e59324dc3127891c704f2d415007fb8356
512511 <2293135461@qq.com>
2022-10-09 14:13:06 +0800

    first commit

---
+++ src/main/java/com/cd/git/testGit.java
@@ -0,0 +1,11 @@
+package com.cd.git;
+
+/**
+ * @author cd512511
+ * @create 2022-10-09 14:09
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
