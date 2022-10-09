3b052ca861a91ac96cf6b3af6d4daff6510731b1
cd512511 <110765318+cd512511@users.noreply.github.com>
2022-10-09 15:03:01 +0800

    pull commit

--- src/main/java/com/cd/git/testGit.java
+++ src/main/java/com/cd/git/testGit.java
@@ -13,5 +13,6 @@ public class testGit {
         System.out.println("master test");
         System.out.println("hpt-fix-test");
         System.out.println("push test");
+         System.out.println("pull test....");
     }
 }
